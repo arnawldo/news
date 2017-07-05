0286045c2dfe12c2fd7d1670145918b802b2cd94
Arnold Taremwa <taremwaarnold@gmail.com>
2017-07-05 11:17:12 +0300

    added test setup, initialize News object test passed

--- src/news.py
+++ src/news.py
@@ -3,4 +3,12 @@ import requests
 class News(object):
     """Class for handling fetching and displaying top news around the world"""
 
-    pass
+    APIKEY = "0146694de85d497187e35ae93a7bafdf"
+
+    def __init__(self):
+        self.sources = ["associated-press", "techcrunch", "bbc-news", "cnn", "bbc-sport", "al-jazeera-english"]
+        self.source_names = ["Associated Press", "TechCrunch", "BBC", "CNN", "BBC Sport", "Al Jazeera English"]
+        self.sort_by = ["top", "latest", "popular"]
+        self.current_source = self.sources[0]
+        self.current_sort = self.sort_by[0]
+        self.articles = []
--- tests/test_news.py
+++ tests/test_news.py
@@ -1,1 +1,14 @@
 import pytest
+
+from src.news import News
+
+@pytest.fixture
+def news_client():
+    return News()
+
+# check initial attributes
+
+def test__news_has_six_news_sources__succeeds(news_client):
+    assert len(news_client.sources) == 6
+    assert len(news_client.source_names) == 6
+
