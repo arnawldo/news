9b5f040e2896a9320f46cc8a2ddaadfa093eaa6d
Arnold Taremwa <taremwaarnold@gmail.com>
2017-07-05 11:21:34 +0300

    added test setup, initialize News object test passed

--- tests/test_news.py
+++ tests/test_news.py
@@ -8,7 +8,13 @@ def news_client():
 
 # check initial attributes
 
-def test__news_has_six_news_sources__succeeds(news_client):
+def test__news_has_six_news_sources__equal(news_client):
     assert len(news_client.sources) == 6
     assert len(news_client.source_names) == 6
+
+def test__news_sources_and_source_names_equal__succeeds(news_client):
+    assert len(news_client.sources) == len(news_client.source_names)
+
+def test__news_class_has_api_key__succeds():
+    assert News.APIKEY is not None
 
